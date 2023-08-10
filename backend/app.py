0444c5456f901c50082d7acfac4941a3c844b23d
hyun <duboo31@gmail.com>
2023-08-10 19:15:09 +0900

    crud 완료 ux 미완료

--- backend/app.py
+++ backend/app.py
@@ -1,31 +1,68 @@
+from pymongo import MongoClient
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
-
-from pymongo import MongoClient
 # 아래 URL이 MongoDB의 주소 connect -> DB의 driver python을 변경하고 버전 3.6 or later로 설정 후 아래에서 복붙하면 됨.
-client = MongoClient('mongodb+srv://sparta:test@cluster0.27ze7vj.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient(
+    'mongodb+srv://sparta:test@cluster0.svk42ds.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
 
-# @app.route("/guestbook", methods=["POST"])
-# def guestbook_post():
-#     name_receive = request.form['name_give']
-#     comment_receive = request.form['comment_give']
-#     doc = {
-#         'name' : name_receive ,
-#         'comment' : comment_receive
-#     }
-#     db.fan.insert_one(doc)
+# 유저 생성 post 요청
 
-#     return jsonify({'msg': '저장 완료!'})
 
-# @app.route("/guestbook", methods=["GET"])
-# def guestbook_get():
-#     all_comments = list(db.fan.find({},{'_id':False}))
-#     return jsonify({'msg': 'GET 완료!'})
+@app.route('/member', methods=['POST'])
+def user_post():
+    name_receive = request.form['name_give']
+
+    doc = {
+        'name': name_receive,
+        'mbti': 'mbtiTest',
+        'stack': ['stackTest'],
+        'tmi': 'tmiTest',
+        'blog': 'blogTest',
+        'github': 'gitTest',
+        'intro': 'introTest',
+        'img': 'imgTest'
+    }
+    db.member.insert_one(doc)
+    print('post 데이터 저장', doc)
+    return jsonify({'msg': 'app.py > post 데이터 저장 완료'})
+
+# 유저 정보 가져오는 get 요청
+
+
+@app.route('/member', methods=['GET'])
+def members_get():
+    all_members = list(db.member.find({}, {'_id': False}))
+    return jsonify({'all_members': all_members})
+
+# 삭제 요청
+
+
+@app.route('/member', methods=['DELETE'])
+def user_delete():
+    name_receive = request.form['name_give']
+    db.member.delete_one({'name': name_receive})
+    return jsonify({'msg': '삭제 완료!'})
+
+
+@app.route('/member_modify', methods=['POST'])
+def user_modify():
+    name_receive = request.form['name_give']
+    att_category_receive = request.form['att_category_give']
+    att_to_receive = request.form['att_to_give']
+    if (att_category_receive == 'stack'): # stack인 경우엔 배열로 저장
+        att_to_receive = request.form['att_to_give']
+        att_to_receive = att_to_receive.split(',') # 클라이언트에서 배열을 전달 받으면 ','로 구분된 문자열 상태로 들어옴.
+    else: # 그 외의 경우엔 문자열로 저장
+        att_to_receive = request.form['att_to_give']
+    db.member.update_one({'name':name_receive},{'$set':{ att_category_receive : att_to_receive }})
+    return jsonify({'msg': '수정 완료!'})
+
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5002, debug=True)
+    app.run('0.0.0.0', port=5002, debug=True)
--- backend/static/js/terminal.js
+++ backend/static/js/terminal.js
@@ -20,3 +20,50 @@ const getCategory = (e) => {
 categoryList.forEach((categoryItem) => {
   categoryItem.addEventListener("click", getCategory);
 });
+
+// 삭제
+const deleteBtn = document.querySelector(".deleteBtn");
+
+const deleteMember = () => {
+  let curSelectMember = document.querySelector(".topTabName").innerText;
+
+  let formData = new FormData();
+
+  formData.append("name_give", curSelectMember);
+  fetch("/member", { method: "DELETE", body: formData })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+  location.reload();
+};
+
+// 수정
+const updateBtn = document.querySelector(".updateBtn");
+
+const updateMember = () => {
+  let curSelectMember = document.querySelector(".topTabName").innerText;
+
+  let currentCategoryVal = document.querySelector(".category").value;
+
+  let updateVal = document.querySelector(".categoryInfo").value;
+
+  let formData = new FormData();
+
+  formData.append("name_give", curSelectMember);
+  formData.append("att_category_give", currentCategoryVal);
+  formData.append("att_to_give", updateVal);
+
+  fetch("/member_modify", { method: "POST", body: formData })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+    });
+  console.log(curSelectMember);
+  console.log(currentCategoryVal);
+  location.reload();
+};
+
+// 삭제 / 수정 이벤트
+deleteBtn.addEventListener("click", deleteMember);
+updateBtn.addEventListener("click", updateMember);
